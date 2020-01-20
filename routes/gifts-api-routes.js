99dc8116567b872a5e42733ff56db71da129dec1
awberez <alex@berez.com>
2020-01-19 21:07:32 -0600

    minor fixes

--- routes/gifts-api-routes.js
+++ routes/gifts-api-routes.js
@@ -1,4 +1,5 @@
-const cheerio = require("cheerio"), request = require("request"), giftsArr = require("../gifts.js");
+const cheerio = require("cheerio"), request = require("request"), giftsArr = require("../gifts.js"),
+customHeaderRequest = request.defaults({ headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'} });
 
 module.exports = (app)=>{
 
@@ -8,20 +9,24 @@ module.exports = (app)=>{
 		for (let object of giftsArr) {
 			if(object.scrape) {
 				scrapeArr.push(object.name);
-				let customHeaderRequest = request.defaults({ headers: {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'} });
 				customHeaderRequest.get(object.link, function(err, resp, body){
-				  	let $ = cheerio.load(body), results = [];
+				  	let $ = cheerio.load(body);
 				  	$("div.inset").each(function(i, element) {
 				    	let size = $(element).children("div.title").text(), price = $(element).children("div.subtitle").text();
 				    	if (size == object.size) {
+				    		
 				    		price = parseFloat(price.substr(1));
 				    		object.sortPrice = price;
 							object.listPrice = "$" + price + " + s&h";
+							
 							scrapeArr = scrapeArr.filter(e => e !== object.name);
-							if (!scrapeArr.length) { res.send(giftsArr); };
+							if (!scrapeArr.length) { 
+								res.send(giftsArr); 
+							};
+				  			
 				  			console.log("price found for " + object.name);
 				  			console.log("$" + price);
-				      	return false;
+				      		return false;
 				    	}
 				  	});
 				});
