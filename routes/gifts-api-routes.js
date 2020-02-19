83004e3d8c6989c8b18f6b22fe63716714f5b819
awberez <alex@berez.com>
2020-02-18 21:35:14 -0600

    minor cleanup

--- routes/gifts-api-routes.js
+++ routes/gifts-api-routes.js
@@ -24,9 +24,7 @@ module.exports = (app)=>{
 		}, 10000);
 		for (let object of scrapeArr) {
 			console.log(`getting price for ${object.name}`);
-			db.PriceList.findOne({
-		        where: { key: object.key }
-		    }).then((dbGift)=>{
+			db.PriceList.findOne({ where: { key: object.key } }).then((dbGift)=>{
 		    	let d = new Date(), timeDif = 15 * 60 * 1000;
 		    	if (!dbGift || dbGift.name !== object.name || d.getTime() - timeDif > Date.parse(dbGift.updatedAt)) {
 		    		console.log(`scraping for ${object.name}`);
@@ -39,46 +37,30 @@ module.exports = (app)=>{
 							    	let size = $(element).children("div.title").text(), price = parseFloat($(element).children("div.subtitle").text().substr(1).replace(/\,/g, ''));
 							    	if (size == object.size && !isNaN(price)) {
 							    		addGift(object, price);
-										console.log(`price found for ${object.name}:\n$${price}`);
-										savePrice(object.key, object.name, price, false);
+										savePrice(object.key, object.name, price, false, `price found for ${object.name}:\n$${price}`);
 									    return false;	
 							    	}
-							    	else if (i == $("div.inset").length - 1) {
-							    		console.log("no price found");
-							    		savePrice(object.key, object.name, 0, true);
-							    	}
+							    	else if (i == $("div.inset").length - 1) { savePrice(object.key, object.name, 0, true, "no price found"); };
 							  	}); 
 							}
 							else if ($("span#priceblock_ourprice")) {
 								let price = parseFloat($("span#priceblock_ourprice").text().substr(1).replace(/\,/g, ''));
 								if (!isNaN(price)) {
 									addGift(object, price);
-									console.log(`price found for ${object.name}:\n$${price}`);
-									savePrice(object.key, object.name, price, false);
+									savePrice(object.key, object.name, price, false, `price found for ${object.name}:\n$${price}`);
 								}
-								else {
-									console.log("no price found");
-							    	savePrice(object.key, object.name, 0, true);
-								};
+								else { savePrice(object.key, object.name, 0, true, "no price found"); };
 							}
-							else {
-								console.log("failed to locate price data");
-								savePrice(object.key, object.name, 0, true);
-							};
+							else { savePrice(object.key, object.name, 0, true, "failed to locate price data"); };
 						}
-						else {
-							console.log("error scraping site");
-							savePrice(object.key, object.name, 0, true);
-						}	
+						else { savePrice(object.key, object.name, 0, true, "error scraping site"); };
 					});
 				}
 				else {
 					console.log(`not scraping for ${object.name}`);
-			   		if (dbGift.no_price) {
-			   			sendGifts(object.key);
-			   		}
+			   		if (dbGift.no_price) { sendGifts(object.key); }
 			   		else {
-			   			addGift(object, dbGift.price)
+			   			addGift(object, dbGift.price);
 						sendGifts(object.key);
 			   		};
 		   		}
@@ -87,7 +69,7 @@ module.exports = (app)=>{
 		addGift = (object, price) => {
 			object.sortPrice = price, object.listPrice = object.link.includes("amazon") ? `$${price % 1 !== 0 ? price.toFixed(2) : price}` : `$${price} + s&h`;
 			sendArr.push(object);
-		}
+		};
 		sendGifts = (key) => {
 			scrapeArr = scrapeArr.filter(e => e.key !== key)
 			if (!scrapeArr.length && !giftsSent) {
@@ -95,17 +77,16 @@ module.exports = (app)=>{
 				res.send(sendArr); 
 				giftsSent = true;
 			}
-		}
-		savePrice = (key, name, price, no_price) => {
+		};
+		savePrice = (key, name, price, no_price, message) => {
+			console.log(message);
 			db.PriceList.upsert({
 				key: key,
 		        name: name,
 		        price: price,
 		        no_price: no_price
-		    }).then(()=>{
-		    	sendGifts(key);
-		    });
-		}
+		    }).then(()=>{ sendGifts(key); });
+		};
 	});
 
 };
